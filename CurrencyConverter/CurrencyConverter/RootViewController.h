904d518ca78410476402c4f6ccb9d44056b2ae43
KitNik <kit.tru.random@gmail.com>
2015-03-29 19:22:52 +0300

    First Commit

---
+++ CurrencyConverter/CurrencyConverter/RootViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CurrencyConverter
+//
+//  Created by Nikita on 29.03.15.
+//  Copyright (c) 2015 Nikita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+
+@end
+
---
+++ CurrencyConverter/CurrencyConverter/RootViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CurrencyConverter
+//
+//  Created by Nikita on 29.03.15.
+//  Copyright (c) 2015 Nikita. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
